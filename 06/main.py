2ea1c5ede8db29fbbcb1cd16d5eda46dd3a0d1b9
Gurdit Singh Siyan <16bitmood@gmail.com>
2021-12-06 14:43:07 +0530

    day 6

--- 06/main.py
+++ 06/main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 from collections import Counter,defaultdict
 with open('data.txt') as f:
-    xs = [int(x) for x in f.read().split(',') if x != '0']
+    xs = [int(x) for x in f.read().split(',')]
     xs = defaultdict(int, dict(Counter(xs)))
 
 def next_day(xs):
     ys = defaultdict(int, {(d-1): c  for d,c in xs.items() if d != 0})
     ys[6] += xs[0]
-    ys[8] += xs[0]
+    ys[8] = xs[0]
     return ys
 
 [xs := next_day(xs) for _ in range(80)]
